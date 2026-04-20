b1ee6e7380a6582767d963cc31b345a1ce6dfb20
luccacardinale <luccarcardinale@gmail.com>
2026-04-20 18:31:41 -0300

    CP2-NovasInterfaces

--- src/interfaces/consulta.ts
+++ src/interfaces/consulta.ts
@@ -1,13 +1,13 @@
-
 import { Medico } from "./medico";
 import { Paciente } from "../types/paciente";
-import { statusConsulta } from "../types/statusConsulta";
+import { StatusConsulta } from "../types/statusConsulta";
+
 export interface Consulta {
   id: number;
   medico: Medico;
   paciente: Paciente;
   data: Date;
   valor: number;
-  status: statusConsulta;
+  status: StatusConsulta;
   observacoes?: string;
 }
--- src/types/statusConsulta.ts
+++ src/types/statusConsulta.ts
@@ -1,4 +1,4 @@
-export type statusConsulta =
+export type StatusConsulta =
   | "agendada"
   | "confirmada"
   | "cancelada"
